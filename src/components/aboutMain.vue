f90a83f438e777f78c7fba73ad9ddc6ac47986b7
AliceInCradle <chenshi@krutt.org>
2024-01-17 13:58:14 +0800

    have a rest first

--- src/components/aboutMain.vue
+++ src/components/aboutMain.vue
@@ -6,25 +6,25 @@
   <div class="row">
     <div class="col-sm-6 col-md-3">
       <a href="#" class="thumbnail">
-        <img src="../../images/bg.jpg"
+        <img src="../../img/bg.jpg"
              alt="通用的占位符缩略图">
       </a>
     </div>
     <div class="col-sm-6 col-md-3">
       <a href="#" class="thumbnail">
-        <img src="../../images/bg.jpg"
+        <img src="../../img/bg.jpg"
              alt="通用的占位符缩略图">
       </a>
     </div>
     <div class="col-sm-6 col-md-3">
       <a href="#" class="thumbnail">
-        <img src="../../images/bg.jpg"
+        <img src="../../img/bg.jpg"
              alt="通用的占位符缩略图">
       </a>
     </div>
     <div class="col-sm-6 col-md-3">
       <a href="#" class="thumbnail">
-        <img src="../../images/bg.jpg"
+        <img src="../../img/bg.jpg"
              alt="通用的占位符缩略图">
       </a>
     </div>
--- src/userLogin.vue
+++ src/userLogin.vue
@@ -1,49 +1,35 @@
 <template>
-  <div class="bg">
-    <div class="welcome">
-      <h1 >欢迎来到NPUSEC！</h1>
-    </div>
-    <div class="login-top">
-      <h2>请登录</h2>
-      <form class="form-horizontal" role="form">
-        <!--第1行：账号-->
-        <div class="form-group">
-          <label for="name" class="col-sm-3 control-label">Username：</label>
-          <div class="col-sm-8">
-            <input type="text" id="name" class="form-control" placeholder="请输入用户名">
+  <div class="box">
+    <div class="content">
+      <div class="login-wrapper">
+        <h1>Login</h1>
+        <div class="login-form">
+          <div class="username form-item">
+            <span>Use your email</span>
+            <input type="text" class="input-item">
+          </div>
+          <div class="password form-item">
+            <span>Input your password</span>
+            <input type="password" class="input-item">
+          </div>
+          <button class="login-btn">Login</button>
+        </div>
+        <div class="divider">
+          <span class="line"></span>
+          <div class="divider-text">another login methods</div>
+          <span class="line"></span>
+        </div>
+        <div class="other-login-wrapper">
+          <div class="other-login-item">
+            <img src="../img/assets/QQ.png" alt="QQ">
+          </div>
+          <div class="other-login-item">
+            <img src="../img/bg.jpg" alt="Wechat">
           </div>
         </div>
-        <!--第2行：密码-->
-        <div class="form-group">
-          <label for="password" class="col-sm-3 control-label">Password：</label>
-          <div class="col-sm-8">
-            <input type="password" id="password" class="form-control" placeholder="请输入密码">
-          </div>
-        </div>
-        <div class="form-group">
-          <div class="col-sm-offset-3 col-sm-5">
-            <div class="checkbox">
-              <label>
-                <input type="checkbox">Remember Me
-              </label>
-            </div>
-          </div>
-        </div>
-        <div class="form-group">
-          <div class="forgot col-sm-offset-2 col-sm-10">
-            <a href="#">forgot Password</a>
-            <input type="submit" value="Login">
-            <!--<button type="submit" class="btn btn-success">登录</button>-->
-          </div>
-        </div>
-      </form>
-    </div>
-    <!--第2框：新用户注册框-->
-    <div class="login-bottom">
-      <h3>New User? &nbsp;<a href="/register">Register</a>&nbsp Here</h3>
+      </div>
     </div>
   </div>
-
 </template>
 
 <script>
@@ -65,121 +51,49 @@ export default {
 </script>
 
 <style scoped>
-/*--1.设置整体背景图、以及全文默认字体大小--*/
+* {
+  margin: 0;
+  padding: 0;
 
-
-/*--2.中间登录框的大小，根据屏幕比例自适应变化--*/
-.bg {
-  width: 200%;
-  margin: 0 auto;
-}
-.bg h2{
-  font-size: 30px;
-  font-weight: 700;
-  color: #fff;
-  text-align: center;
-  margin: 0 0 50px 0;
 }
 
-/*--3.登录框上半部分--*/
-.login-top {
-  background-color:#E1E1E1 ;
-  border-radius: 25px 25px 0 0;
-  padding: 40px 60px;
-  opacity:0.9;
+.box {
+  width: 100vw;
+  height: 100vh;
+  background-color: rgb(29, 67, 89);
+  .content{
+    .login-wrapper {
+      h1 {
+        text-align: center;
+      }
+      .login-form {
+        .form-item {
+          margin: 20px 0;
+          span {
+            display: block;
+            margin: 5px 20px;
+            font-weight: 100;
+          }
+          .input-item {
+            width: 100%;
+            border-radius: 40px;
+            padding: 20px;
+            box-sizing: border-box;
+            font-size: 20px;
+            font-weight: 200;
+            &:focus {
+              outline: none;
+            }
+          }
+        }
+        .login-btn {
+          width: 100%;
+          border-radius: 40px;
+          color: #ffffff;
+          
+        }
+      }
+    }
+  }
 }
-.login-top h1 {
-  text-align: center;
-  font-size: 27px;
-  font-weight: 500;
-  color: #ADD8E6;
-  margin: 0 0 20px 0;
-}
-
-/*3.2.账户输入框的样式自定义*/
-.login-top input[type="text"] {
-  outline: none;
-  font-size: 15px;
-  font-weight: 500;
-  color: #818181;
-  padding: 1px 20px;
-  background: #CACACA;
-  border: 1px solid #ccc;
-  border-radius:25px;
-  margin: 0 0 12px 0;
-  width: 88%;
-}
-
-/*3.3.密码输入框的样式自定义*/
-.login-top input[type="password"]{
-  outline: none;
-  font-size: 15px;
-  font-weight: 500;
-  color: #818181;
-  padding: 1px 20px;
-  background: #CACACA;
-  border: 1px solid #ccc;
-  border-radius:25px;
-  margin: 0 0 12px 0;
-  width: 88%;
-}
-
-/*3.4.忘记密码*/
-.forgot  a{
-  font-size: 13px;
-  font-weight: 500;
-  color: #F45B4B;
-  display: inline-block;
-  border-right: 2px solid #ADD8E6;
-  padding: 0 7px 0 0;
-}
-.forgot  a:hover{
-  color: #818181;
-}
-
-.forgot input[type="submit"] {
-  background: #ADD8E6;
-  color: #FFF;
-  font-size: 17px;
-  font-weight: 400;
-  padding: 8px 7px;
-  width: 20%;
-  display: inline-block;
-  cursor: pointer;
-  border-radius: 6px;
-  margin: 0 7px 0 3px;
-  outline: none;
-  border: none;
-}
-.forgot input[type="submit"]:hover {
-  background:#818181;
-  transition: 0.5s all;
-}
-
-.login-bottom {
-  background: #ADD8E6;
-  padding: 20px 65px;
-  border-radius: 0 0 25px 25px;
-  text-align: right;
-  border-top: 2px solid #ADD8E6;
-}
-.login-bottom h3 {
-  font-size: 14px;
-  font-weight: 500;
-  color: #fff;
-}
-.login-bottom h3 a {
-  font-size: 25px;
-  font-weight: 500;
-  color: #fff;
-}
-.login-bottom h3 a:hover {
-  color:#696969;
-  transition: 0.5s all;/*颜色渐变效果*/
-}
-
-.welcome {
-  justify-content: center;
-}
-
 </style>
